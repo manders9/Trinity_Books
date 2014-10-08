580785f0f63f8529a666871fe163b472f1b49a8b
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-08 15:53:21 -0400

    Identity model, controller, views and specs added.

---
+++ app/controllers/identities_controller.rb
@@ -0,0 +1,42 @@
+class IdentitiesController < ApplicationController
+def index
+    @identities = current_user.identities if current_user
+  end
+
+  def all
+    omniauth = request.env["omniauth.auth"]
+    identity = Identity.find_by_provider_and_uid(
+      omniauth['provider'],
+      omniauth['uid']
+      )
+    if identity
+      flash[:notice] = "Signed in successfully."
+      sign_in_and_redirect(:user, identity.user)
+    elsif current_user
+      current_user.identities.create!(
+        provider: omniauth['provider'],
+        uid: omniauth['uid']
+        )
+      flash[:notice] = "Authentication successful."
+      redirect_to identities_url
+    else
+      user = User.new
+      user.apply_omniauth(omniauth)
+      if user.save
+        flash[:notice] = "Signed in successfully."
+        sign_in_and_redirect_to root_url
+      else
+        session[:omniauth] = omniauth.except('extra')
+        redirect_to new_user_registration_url
+      end
+    end
+  end
+  alias_method :twitter, :all
+
+  def destroy
+    @identity = current_user.identities.find(params[:id])
+    @identity.destroy
+    flash[:notice] = "Successfully destroyed authentication."
+    redirect_to identities_url
+  end
+end
