e86cbfc5ba3684d1733fd65fafadc350615d04a1
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-08 17:25:45 -0400

    User model identity associations and methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,23 @@
 class User < ActiveRecord::Base
+  has_many :identities, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :omniauthable, :confirmable
 
   validates_uniqueness_of :username
+
+  def apply_omniauth(omniauth)
+    identities.build(provider: :omniauth['provider'], uid: :omniauth['uid'])
+  end
+
+  def password_required?
+    (identities.empty? || !password.blank?) && super
+  end
+
+  def update_with_password(params, *options)
+    update_attributes(params, *options)
+  end
 end
