4946e9b3552b1fd2f1cc3c8771a6f99951dae16c
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-08 17:25:10 -0400

    Application controller modified.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def authorize!
-    if current_user.nil? || !current_user.admin?
-      flash[:notice] = "You must be an admin to access this!"
-      redirect_to root_path
-    end
-  end
+  # def authorize!
+  #   if current_user.nil? || !current_user.admin?
+  #     flash[:notice] = "You must be an admin to access this!"
+  #     redirect_to root_path
+  #   end
+  # end
 
   protected
 
@@ -17,4 +17,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << [:username]
     # devise_parameter_sanitizer.for(:account_update) << [:profile_photo]
   end
+
+  # def ensure_signup_complete
+  #   # Ensure we don't go into an infinite loop
+  #   return if action_name == 'finish_signup'
+
+  #   # Redirect to the 'finish_signup' page if the user
+  #   # email hasn't been verified yet
+  #   if current_user && !current_user.email_verified?
+  #     redirect_to finish_signup_path(current_user)
+  #   end
+  # end
 end
