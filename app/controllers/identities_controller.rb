3dc6bd42348371d4292a129bd17231b25d443cc6
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-09 00:20:44 -0400

    Omniauth configuration.

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,24 +1,18 @@
 class IdentitiesController < ApplicationController
-def index
+  def index
     @identities = current_user.identities if current_user
   end
 
   def all
     omniauth = request.env["omniauth.auth"]
-    identity = Identity.find_by_provider_and_uid(
-      omniauth['provider'],
-      omniauth['uid']
-      )
+    identity = Identity.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
     if identity
       flash[:notice] = "Signed in successfully."
       sign_in_and_redirect(:user, identity.user)
     elsif current_user
-      current_user.identities.create!(
-        provider: omniauth['provider'],
-        uid: omniauth['uid']
-        )
+      current_user.identities.create!(identity_params)
       flash[:notice] = "Authentication successful."
-      redirect_to identities_url
+      redirect_to root_path
     else
       user = User.new
       user.apply_omniauth(omniauth)
@@ -37,6 +31,15 @@ def index
     @identity = current_user.identities.find(params[:id])
     @identity.destroy
     flash[:notice] = "Successfully destroyed authentication."
-    redirect_to identities_url
+    redirect_to root_path
+  end
+
+  private
+
+  def identity_params
+    params.require(:identity).permit(
+      :provider,
+      :uid
+      )
   end
 end
