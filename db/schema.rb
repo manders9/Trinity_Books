4c5a2d0e4bdb012db1a0c797d8c74abbf42910f0
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-09 00:18:36 -0400

    Confirmable added to devise.

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,12 @@ ActiveRecord::Schema.define(version: 20141008164908) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username",                            null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
