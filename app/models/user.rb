7206598e36eac1f76f19b31a1bdfb2f72cc5eb3f
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-09 15:32:50 -0400

    User model devise formatting edited.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,26 +3,69 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
-         :validatable, :omniauthable, :confirmable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable,
+         :omniauthable,
+         omniauth_providers: [
+          :facebook,
+          :twitter,
+          :google_oauth2
+          ]
 
   validates_uniqueness_of :username
 
-  after_create :send_email
-  def send_email
-    UserMailer.welcome_email(self).deliver
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.username = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+    end
   end
 
-  def apply_omniauth(omniauth)
-    identities.build(provider: omniauth['provider'], uid: omniauth['uid'])
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+        user.username = date["name"] if user.username.blank?
+      end
+    end
   end
 
-  def password_required?
-    (identities.empty? || !password.blank?) && super
-  end
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(email: data["email"]).first
 
-  def update_with_password(params)
-    update_attributes(user_params)
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+      user = User.create(
+        username: data["name"],
+        email: data["email"],
+        password: Devise.friendly_token[0,20]
+        )
+    end
+    user
   end
 end
+
+  # after_create :send_email
+  # def send_email
+  #   UserMailer.welcome_email(self).deliver
+  # end
+
+  # def apply_omniauth(omniauth)
+  #   identities.build(provider: omniauth['provider'], uid: omniauth['uid'])
+  # end
+
+  # def password_required?
+  #   (identities.empty? || !password.blank?) && super
+  # end
+
+  # def update_with_password(params)
+  #   update_attributes(user_params)
+  # end
