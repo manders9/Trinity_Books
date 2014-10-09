0dd63d963daee78d50960e862cb9d7e9a8736a6c
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-09 15:31:16 -0400

    Identities and registrations removed.

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,45 +1,48 @@
-class IdentitiesController < ApplicationController
-  def index
-    @identities = current_user.identities if current_user
-  end
+# class IdentitiesController < ApplicationController
+#   def index
+#     @identities = current_user.identities if current_user
+#   end
 
-  def all
-    omniauth = request.env["omniauth.auth"]
-    identity = Identity.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
-    if identity
-      flash[:notice] = "Signed in successfully."
-      sign_in_and_redirect(:user, identity.user)
-    elsif current_user
-      current_user.identities.create!(identity_params)
-      flash[:notice] = "Authentication successful."
-      redirect_to root_path
-    else
-      user = User.new
-      user.apply_omniauth(omniauth)
-      if user.save
-        flash[:notice] = "Signed in successfully."
-        sign_in_and_redirect_to root_url
-      else
-        session[:omniauth] = omniauth.except('extra')
-        redirect_to new_user_registration_url
-      end
-    end
-  end
-  alias_method :twitter, :all
+#   def all
+#     omniauth = request.env["omniauth.auth"]
+#     identity = Identity.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
+#     if identity
+#       flash[:notice] = "Signed in successfully."
+#       sign_in_and_redirect(:user, identity.user)
+#     elsif current_user
+#       current_user.identities.create!(identity_params)
+#       flash[:notice] = "Authentication successful."
+#       redirect_to root_path
+#     else
+#       user = User.new
+#       user.apply_omniauth(omniauth)
+#       if user.save
+#         flash[:notice] = "Signed in successfully."
+#         UserMailer.welcome_email(user).deliver
+#         sign_in_and_redirect_to root_url
+#       else
+#         session[:omniauth] = omniauth.except('extra')
+#         redirect_to new_user_registration_url
+#       end
+#     end
+#   end
+#   alias_method :twitter, :all
+#   alias_method :facebook, :all
+#   alias_method :google_oauth2, :all
 
-  def destroy
-    @identity = current_user.identities.find(params[:id])
-    @identity.destroy
-    flash[:notice] = "Successfully destroyed authentication."
-    redirect_to root_path
-  end
+#   def destroy
+#     @identity = current_user.identities.find(params[:id])
+#     @identity.destroy
+#     flash[:notice] = "Successfully destroyed authentication."
+#     redirect_to root_path
+#   end
 
-  private
+#   private
 
-  def identity_params
-    params.require(:identity).permit(
-      :provider,
-      :uid
-      )
-  end
-end
+#   def identity_params
+#     params.require(:identity).permit(
+#       :provider,
+#       :uid
+#       )
+#   end
+# end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,22 +1,22 @@
-class RegistrationsController < Devise::RegistrationsController
-  def edit
-    @identities = current_user.identities if current_user
-    super
-  end
+# class RegistrationsController < Devise::RegistrationsController
+#   def edit
+#     @identities = current_user.identities if current_user
+#     super
+#   end
 
-  def create
-    super
-    session[:omniauth] = nil unless @user.new_record?
-  end
+#   def create
+#     super
+#     session[:omniauth] = nil unless @user.new_record?
+#   end
 
-  private
+#   private
 
-  def build_resource(*args)
-    super
-    if session[:omniauth]
-      @user.apply_omniauth(session[:omniauth])
-      @user.valid?
-    end
-  end
+#   def build_resource(*args)
+#     super
+#     if session[:omniauth]
+#       @user.apply_omniauth(session[:omniauth])
+#       @user.valid?
+#     end
+#   end
 
-end
+# end
