190e21ee71d6bf237f8e7a642ce9df264ef1bb59
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-09 00:22:25 -0400

    User params established.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :username,
+      :email,
+      :password,
+      :password_confirmation,
+      :remember_me
+      )
+  end
 end
