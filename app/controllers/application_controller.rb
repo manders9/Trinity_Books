dce3a55f790a92ddaaea1c4b988c976382754d54
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-09 15:31:30 -0400

    Ensure signup removed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :authenticate_user!
 
   # def authorize!
   #   if current_user.nil? || !current_user.admin?
@@ -17,15 +18,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << [:username]
     # devise_parameter_sanitizer.for(:account_update) << [:profile_photo]
   end
-
-  # def ensure_signup_complete
-  #   # Ensure we don't go into an infinite loop
-  #   return if action_name == 'finish_signup'
-
-  #   # Redirect to the 'finish_signup' page if the user
-  #   # email hasn't been verified yet
-  #   if current_user && !current_user.email_verified?
-  #     redirect_to finish_signup_path(current_user)
-  #   end
-  # end
 end
