c97736f908bdf8222ad235739aff01a95e50448b
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-09 16:38:32 -0400

    Identity specs commented out.

--- spec/factories/identities.rb
+++ spec/factories/identities.rb
@@ -1,9 +1,9 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do
-  factory :identity do
-    user nil
-    provider "MyString"
-    uid "MyString"
-  end
-end
+# FactoryGirl.define do
+#   factory :identity do
+#     user nil
+#     provider "MyString"
+#     uid "MyString"
+#   end
+# end
