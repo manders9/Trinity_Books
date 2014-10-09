55e31ba0e7a0144ef57ab0d6ca7c1482b37e7dc8
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-09 16:38:47 -0400

    Omniauth added to spec rails helper.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,7 +17,19 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+OmniAuth.config.test_mode = true
+OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+  :provider => 'facebook',
+  :uid => '123123123123',
+  :info => {
+    :email => "sbanskota08@gmail.com",
+    :nickname => "sarupbanskota"
+  }
+})
+
 RSpec.configure do |config|
+  config.include Devise::TestHelpers, type: :controller
+  config.include Features::SessionHelpers, type: :feature
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
